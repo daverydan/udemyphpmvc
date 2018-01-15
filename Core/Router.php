5b6174636b69d2df6101dac5a51604e82b2c136a
Danny Avery <danny.w.avery@gmail.com>
2018-01-15 10:06:03 -0700

    Basic route matching using params

--- Core/Router.php
+++ Core/Router.php
@@ -8,6 +8,12 @@ class Router {
 	protected $routes = [];
 
 	/**
+	 * Associative array of routes (the routing table)
+	 * @var array
+	 */
+	protected $params = [];
+
+	/**
 	 * Add a route to the routing table
 	 * 
 	 * @param string $route The route URL
@@ -28,5 +34,34 @@ class Router {
 	public function getRoutes()
 	{
 		return $this->routes;
+	}
+
+	/**
+	 * Match the routes to the routes in the routing table, setting the $params
+	 * property if a route is found
+	 * 
+	 * @param string $url The route URL
+	 * 
+	 * @return boolean true if a match found, false otherwise
+	 */
+	public function match($url)
+	{
+		foreach ($this->routes as $route => $params) {
+			if ($url == $route) {
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Get the currently matched parameters
+	 * 
+	 * @return array
+	 */
+	public function getParams()
+	{
+		return $this->params;
 	}
 }
