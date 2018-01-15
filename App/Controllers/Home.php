cbef5e2b9bb1e8138f92c37a79a12b64ea85ecfb
Danny Avery <danny.w.avery@gmail.com>
2018-01-15 12:40:02 -0700

    Load classes automatically: add namespaces & an autoload function

---
+++ App/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Home Controller
+ */
+class Home {
+
+	/**
+	 * Show index page
+	 * 
+	 * @return void
+	 */
+	public function index()
+	{
+		echo 'HomeController @ index()';
+	}
+}
--- public/index.php
+++ public/index.php
@@ -7,14 +7,25 @@
  */
 
 // Require the controller class
-require '../App/Controllers/Posts.php';
+// require '../App/Controllers/Posts.php';
+
+/**
+ * Autoloader for classes
+ */
+spl_autoload_register(function($class){
+	$root = dirname(__DIR__); // get the parent directory
+	$file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+	if (is_readable($file)) {
+		require $root . '/' . str_replace('\\', '/', $class) . '.php';
+	}
+});
 
 /**
  * Routing
  */
-require '../Core/Router.php';
+// require '../Core/Router.php';
 
-$router = new Router();
+$router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
